c68fad4c250838f31cb0aaf98003006b813e6f2d
CelestialKittieAnnie <111916305+CelestialKittieAnnie@users.noreply.github.com>
2026-08-08 23:06:11 -0400

    More Interactive JS

--- assets/js/choose_faction.js
+++ assets/js/choose_faction.js
@@ -1,5 +1,15 @@
 const allSections = document.querySelectorAll("section");
 
+const questItems = document.querySelectorAll('li');
+
+function handleItemClick() {
+  this.classList.toggle('done');
+}
+
+questItems.forEach(function (item) {
+  item.addEventListener('click', handleItemClick);
+});
+
 document.body.style.backgroundColor = "black";
 
 console.log(allSections);
